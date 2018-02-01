5d61e4804cb5baebfd27ee965385b7be9d9f1633
Sugeria <Suger@dislab.nju.edu.cn>
2018-02-02 00:13:22 +0800

    judge error task is completed

--- dynamiccloudsim/trunk/src/org/workflowsim/WorkflowScheduler.java
+++ dynamiccloudsim/trunk/src/org/workflowsim/WorkflowScheduler.java
@@ -2436,6 +2436,10 @@ public class WorkflowScheduler extends DatacenterBroker {
 			// new original
 		}else {
 			
+			if(!tasks.containsKey(task.getCloudletId())) {
+				return;
+			}
+			
 			LinkedList<Task> speculativeTaskOfTask = (LinkedList<Task>)speculativeTasks.remove(task.getCloudletId());
 			
 			
