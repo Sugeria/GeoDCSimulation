8d722c4a018d7253947ef0f3968dfa9d90f2ddb2
Sugeria <Suger@dislab.nju.edu.cn>
2018-02-07 16:17:06 +0800

    change totalRunIndex condition in WorkflowEngine

--- dynamiccloudsim/trunk/src/org/workflowsim/WorkflowEngine.java
+++ dynamiccloudsim/trunk/src/org/workflowsim/WorkflowEngine.java
@@ -381,7 +381,8 @@ public final class WorkflowEngine extends SimEntity {
         if (getJobsList().isEmpty() && jobsSubmitted == 0 && isAllDCFail == false && CloudSim.futureSize() == 0) {
             
         	
-        	if(CloudSim.totalRunIndex < 65) {
+        	if((Parameters.isRunExeList == false && CloudSim.totalRunIndex < 65)
+					|| (Parameters.isRunExeList == true && Parameters.exeIndex < (Parameters.exelist.length - 1))) {
 //        		List<Job> outputList0 = getJobsReceivedList();
 //        		WorkflowExample.sortJobId(outputList0);
 //				WorkflowExample.record(outputList0);
